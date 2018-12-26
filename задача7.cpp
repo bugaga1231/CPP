06d8d3895a9898bcf2dd4d60d147da47a0c9180e
bugaga1231 <45533221+bugaga1231@users.noreply.github.com>
2018-12-26 22:13:10 +0300

    Update задача7.cpp

--- задача7.cpp
+++ задача7.cpp
@@ -1,35 +1,43 @@
-#include <iostream>
+// ConsoleApplication8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
-int main(){
-  int kol, n;
-  string s;
-  getline(cin, s);
-  kol = 0;
-  for(int i = 0; i < s.length(); i++){
-    if ((s[i] >= '0') && (s[i] <= '9')){
-      if (i == 0)
-      {
-        j = i + 1;
-        while(s[j] != ' '){
-          j++;
-        }
-        if ((s[j-1] >='0') && (s[j-1] <= '9')){
-          kol++;
-        }
-        i = j;
-      }
-    }
-    else{
-      if(s[i-1] == ' '){
-        j = i + 1;
-        while(s[j] != ' '){
-          j++;
-        }
-        if((s[j-1] >= '0') && (s[j-1] <='9')) {
-          kol++;
-        }
-        i = j;
-      }
-    }
-  cout << kol;
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int j, kol, n;
+	string s;
+	getline(cin, s);
+	kol = 0;
+	for (int i = 0; i < s.length(); i++) {
+		if ((s[i] >= '0') && (s[i] <= '9')) {
+			if (i == 0)
+			{
+				j = i + 1;
+				while (s[j] != ' ') {
+					j++;
+				}
+				if ((s[j - 1] >= '0') && (s[j - 1] <= '9')) {
+					kol++;
+				}
+				i = j;
+			}
+		}
+		else {
+			if (s[i - 1] == ' ') {
+				j = i + 1;
+				while (s[j] != ' ') {
+					j++;
+				}
+				if ((s[j - 1] >= '0') && (s[j - 1] <= '9')) {
+					kol++;
+				}
+				i = j;
+			}
+		}
+		
+	}
+	cout << kol;
 }

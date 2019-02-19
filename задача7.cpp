ad359b61163742e021322725b8d25db098183b73
bugaga1231 <45533221+bugaga1231@users.noreply.github.com>
2019-02-19 19:45:06 +0300

    Update задача7.cpp

--- задача7.cpp
+++ задача7.cpp
@@ -1,43 +1,34 @@
-// ConsoleApplication8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+bool isLetterOrDigit(char c) {
+	return ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
+}
+
 int main() {
-	int j, kol, n;
-	string s;
-	getline(cin, s);
-	kol = 0;
-	for (int i = 0; i < s.length(); i++) {
-		if ((s[i] >= '0') && (s[i] <= '9')) {
-			if (i == 0)
-			{
-				j = i + 1;
-				while (s[j] != ' ') {
-					j++;
-				}
-				if ((s[j - 1] >= '0') && (s[j - 1] <= '9')) {
-					kol++;
-				}
-				i = j;
-			}
-		}
-		else {
-			if (s[i - 1] == ' ') {
-				j = i + 1;
-				while (s[j] != ' ') {
-					j++;
-				}
-				if ((s[j - 1] >= '0') && (s[j - 1] <= '9')) {
-					kol++;
-				}
-				i = j;
-			}
-		}
+	string s = " ", input;
+	getline(cin, input);
+	s += input + " "; // чтобы начало и конец были пробелами
+
+	int res = 0;
+
+	int wordStart, wordEnd;
+	for (int i = 0; i < s.length() - 1; ++i) {
+		if (s[i] == ' ' && isLetterOrDigit(s[i + 1]))
+			wordStart = i + 1;
 		
+		// нашли слово
+		if (isLetterOrDigit(s[i]) && s[i + 1] == ' ') {
+			wordEnd = i;
+			
+			if ('0' <= s[wordStart] && s[wordStart] <= '9' && '0' <= s[wordEnd] && s[wordEnd] <= '9')
+				++res;
+		}
 	}
-	cout << kol;
+
+	cout << res << endl;
+
+	return 0;
 }

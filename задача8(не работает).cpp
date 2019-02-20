ccaf7ce3446fbe45815bc45e556d6893e4359181
bugaga1231 <45533221+bugaga1231@users.noreply.github.com>
2019-02-20 08:42:59 +0300

    Update задача8(не работает).cpp

--- задача8(не работает).cpp
+++ задача8(не работает).cpp
@@ -1,74 +1,96 @@
-#include "stdafx.h"
-#include <string>
-#include <fstream>
 #include <iostream>
-
+#include <fstream>
+#include <string>
+ 
 using namespace std;
-
+ 
+// считает сколько каких цифр
+int* countDigits(string s) {
+	int *res = new int[10];
+	for (int i = 0; i < 10; ++i)
+		res[i] = 0;
+ 
+	for (int i = 0; i < s.length(); ++i) {
+		++res[s[i] - '0'];
+	}
+ 
+	return res;
+}
+ 
+string buildNumber(string s) {
+	string res = "";
+ 
+	// считаем, сколько пар цифр
+	int *evenDigsCount = new int[10];
+	for (int i = 0; i < 10; ++i)
+		evenDigsCount[i] = 0;
+ 
+	// здесь будут нечетные остатки
+	int *oddDigsCount = countDigits(s);
+ 
+	for (int i = 0; i < 10; ++i) {
+		if (oddDigsCount[i] > 0) {
+			evenDigsCount[i] = oddDigsCount[i] / 2; // сколько пар чисел есть
+			oddDigsCount[i] = oddDigsCount[i] % 2; // остаток после того как отняли пары (0 или 1)
+		}
+	}
+ 
+	// дописываем в середину нолики (ВСЕ пары)
+	for (int i = 0; i < evenDigsCount[0]; ++i) {
+		res += "00";
+	}
+ 
+	for (int i = 9; i > 0; --i) {
+		for (int j = 0; j < evenDigsCount[i]; ++j) {
+			char c = '0' + i;
+			res = c + res + c; // приписываем слева и справа цифры
+		}
+	}
+ 
+	// дописываем самую маленькую цифру в середину
+	int i;
+	for (i = 0; i < 10; ++i) {
+		if (oddDigsCount[i]) // если не 0, то нашли самую маленькую цифру
+			break;
+	}
+ 
+	char c = '0' + i;
+	res.insert(s.length() / 2, 1, c);
+ 
+	return res;
+}
+ 
 int main() {
-	int count = 0;
-	int n;
-	fstream F, G;
-	string s;
-	F.open("F.txt", ios::out);
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-		F << s;
+	string in, out;
+	cout << "Enter input file path:" << endl;
+	getline(cin, in);
+	cout << "Enter output file path:" << endl;
+	getline(cin, out);
+ 
+	ifstream f(in);
+	ofstream g(out);
+ 
+	if (!f || !g) {
+		cout << "Error, blyat";
+		return -1;
 	}
-	F.close();
-
-	F.open("F.txt", ios::in);
-	G.open("G.txt", ios::out); //Привязываем и открываем файлы
-	
-	string SG = { " " };
-	int Col = 1;
-	char NumberChar;
-	int Mas[10] = { 0 }; // 2 Массива 1 кол-во i  цифр в Mas[i] и кол-во этих же цифр поделеных на 2
-	int Mas1[10] = { 0 };
-	int Number;
-	while (!F.eof()) {
-		for (int i = 1; i < 10; i++) {
-			Mas[i] = { 0 };
-			Mas1[i] = { 0 };
-		}
-		Col = 1;
-		getline(F, s);
-		cout << s << endl;
-		for (int i = 0; i < size(s); i++) {
-			NumberChar = s.operator[](i); // В NumberChar символ, в Number само число из NumberChar 
-			Number = (NumberChar & 0x0F);
-			Mas[Number]++;
-		}
-		Number = 0;
-		for (int i = 1; i < 10; i++) {
-			NumberChar = i;
-			while (Mas[i] >= 2) {
-				Number = Number + i * Col;
-				Col = Col * 10;
-				Mas1[i]++;
-				Mas[i] -= 2; // Составляем первую половину числа , и запоминаем сколько чисел мы уже записали 
+ 
+	while (!f.eof()) {
+		string line, nums = "";
+		getline(f, line);
+ 
+		for (int i = 0; i < line.length(); ++i) {
+			if ('0' <= line[i] && line[i] <= '9') {
+				nums += line[i];
 			}
 		}
-		for (int i = 9; i > 0; i--) {
-			while (Mas1[i] >= 1) {
-				Number = Number + i * Col;
-				Col = Col * 10;
-				Mas1[i]--; // Дописываем остальную часть числа 
-			}
-		}
-
-		G << Number << endl;
-		SG.clear();
+ 
+		string res = buildNumber(nums);
+		g << res << endl;
 	}
-	F.close();
-	G.close();
-
-	G.open("G.txt", ios::in);
-	for (int i = 0; i < n; i++) {
-		getline(G, s);
-		cout << s;
-	}
-	system("pause");
+ 
+	f.close();
+	g.close();
+ 
 	return 0;
 }
